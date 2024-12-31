44041bcf1ce1ec08d9f69c316318a90ce8436fea
v.popova <violipopova@gmail.com>
2024-12-31 03:57:52 +0200

    feat(Snowflake.tsx): moved getConfig function to a separate utils file to keep the code organised

--- src/components/snowflake/Snowflake.tsx
+++ src/components/snowflake/Snowflake.tsx
@@ -2,9 +2,9 @@ import React, { useRef, useEffect, useState, useMemo } from 'react';
 import {Animated, Easing} from 'react-native';
 import { SnowflakeConfig, SnowflakeProps } from './props';
 import { SnowflakeText } from './styles';
+import { getConfig } from './utils';
 
 const START_Y_POSITION = -50;
-const SNOWFLAKE_TYPES = ['❄', '❅', '❆'];
 
 export default function Snowflake({ scene }: SnowflakeProps) {
   const [config, setConfig] = useState<SnowflakeConfig>(() => getConfig(scene));
@@ -91,35 +91,4 @@ export default function Snowflake({ scene }: SnowflakeProps) {
   );
 }
 
-function getConfig(scene: { width: number; height: number }): SnowflakeConfig {
-  const size = randomInt(5, 10);
-  const opacity = randomInt(4, 10) / 10;
-  const type = SNOWFLAKE_TYPES[randomInt(0, 2)];
-  const xPosition = randomInt(0, scene.width);
 
-  const fallDuration = randomInt(10000, 30000);
-  const fallDelay = randomInt(500, 10000);
-
-  const rotationDuration = randomInt(2000, 10000);
-  const rotationDirection = randomInt(0, 1);
-
-  const swingDuration = randomInt(3000, 8000);
-  const swingAmplitude = randomInt(0, 30);
-
-  return {
-    size,
-    opacity,
-    type,
-    xPosition,
-    fallDelay,
-    fallDuration,
-    rotationDuration,
-    rotationDirection,
-    swingDuration,
-    swingAmplitude,
-  };
-}
-
-function randomInt(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
